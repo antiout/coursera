6dda1090e85589f7e3f207c1a0c4720aa132c854
antiout <uzbssr@gmail.com>
2014-06-20 01:52:30 -0400

    ex

--- scala/example/src/main/scala/example/Lists.scala
+++ scala/example/src/main/scala/example/Lists.scala
@@ -23,7 +23,9 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-  def sum(xs: List[Int]): Int = ???
+  def sum(xs: List[Int]): Int = {
+    if ( xs.isEmpty) 0 else xs.head + sum(xs.tail)
+  }
 
   /**
    * This method returns the largest element in a list of integers. If the
@@ -38,5 +40,10 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def max(xs: List[Int]): Int = ???
+  def max(xs: List[Int]): Int = {
+    def iter(m:Int, list:List[Int]) : Int = {
+      if ( list.isEmpty ) m else iter(m max list.head, list.tail)
+    }
+    iter(xs.head, xs.tail)
+  }
 }
